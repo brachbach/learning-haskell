587af5fe87f1885146ca3583327507de8164ab9b
Ben Rachbach <brachbach@gmail.com>
2018-12-25 17:11:11 -0700

    This set up seems to more or less work, but program needs to be more efficient to run in a reasonable amount of time

--- advent5-1.hs
+++ advent5-1.hs
@@ -1,10 +1,24 @@
 import Data.Char
+-- import System.IO
+-- import qualified Data.Text as T
+
+-- main = do  
+--     contents <- readFile "input.txt"  
+--     putStr ((reactAllPairs contents))
+
+poly = "CXxRrcWhHoOwWtTwSsRApPWwarlYyFWHhJjJjbBTsSQqtBTtbTtwfoEeivVTtIOLwftTFQqWlaFfAGgoOMmQqyYwWWDdwyYnNBNnGgBbwWbiIZAappFjJfPYypPPzTtWwrYyRBuUbjUuJlLKhbBdDAaHkNnKZzKuUkzOoSXxnNsZtTfFIiUuHhkrRvNbBgGneAaEsCcSYyDOodVAanzOoZNJKkpPoOjXaRrAmMfFDaAdtTOfBbFAaFaobBOIiAFfUulLWwlLoOEBbRfyGgYNnFPOopgGhHLll"
 
 swapCase :: Char -> Char
 swapCase letter = if letter == toUpper(letter)
     then toLower(letter)
     else toUpper(letter)
-findFirstPair :: [Char] -> Int
-findFirstPair polymer = (take 1 [i | (letter, i) <- zip polymer [0..], polymer !! (i + 1) == swapCase letter]) !! 0
--- reactFirstPair :: [Char] -> [Char]
--- reactFirstPair polymer =  
+findPairs :: [Char] -> [Int]
+findPairs polymer = [i | (letter, i) <- zip polymer [0..], i < (length polymer) - 1, polymer !! (i + 1) == swapCase letter]
+removeNChars :: Int -> Int -> [Char] -> [Char]
+removeNChars start n charArray = take start charArray ++ drop (start + n) charArray
+reactFirstPair :: [Char] -> [Char] 
+reactFirstPair polymer = removeNChars ((findPairs polymer) !! 0) 2 polymer
+reactAllPairs :: [Char] -> [Char]
+reactAllPairs polymer = if length (findPairs polymer) == 0
+    then polymer
+    else reactAllPairs(reactFirstPair polymer)
