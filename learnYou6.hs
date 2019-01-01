cf9ea482f18228fae8035bbfa127037152e5025f
Ben Rachbach <brachbach@gmail.com>
2019-01-01 16:04:40 -0700

    Success! This was great, learned a lot about how the types work

--- learnYou6.hs
+++ learnYou6.hs
@@ -1,10 +1,10 @@
 zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' _ [] _ = []
 zipWith' _ _ [] = []
-zipWith' f (x:xs) (y:ys) = f x y : zipWith' xs ys
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 
-makeSublist :: a -> b -> [c]
-makeSublist a b = [a,b] 
+makeSublist :: a -> a -> [a]
+makeSublist x y = [x, y] 
 
-zip' :: [a] -> [b] -> [c]
+zip' :: [a] -> [a] -> [[a]]
 zip' listA listB = zipWith makeSublist listA listB
