ca3d1c94e80381cf0d8de86ce3fa3b2a009a0ee0
Ben Rachbach <brachbach@gmail.com>
2019-01-30 16:49:02 -0800

    add collatz chain fn

--- learnYou6.hs
+++ learnYou6.hs
@@ -8,3 +8,19 @@ makeSublist x y = [x, y]
 
 zip' :: [a] -> [a] -> [[a]]
 zip' listA listB = zipWith makeSublist listA listB
+
+map' :: (a -> b) -> [a] -> [b]
+map' f [] = []
+map' f (x:xs) = f x : map' f xs
+
+listify :: a -> [a]
+listify a = [a]
+
+makeCollatzChain :: (Integral a) => a -> [a]
+makeCollatzChain 1 = [1]
+makeCollatzChain x
+    | even x = x : makeCollatzChain(x `div` 2)
+    | otherwise = x : makeCollatzChain((x*3) + 1)
+-- howManyLongerThan :: a -> a -> b
+howManyLongerThan compLength maxNum = length [startNum | startNum <- [1..(maxNum + 1)], length(makeCollatzChain startNum) > compLength]
+
